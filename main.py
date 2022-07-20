01216ef30ee471a86c108f67d1d7afc2dc936851
seif7 <seif.abdelkefi@gmail.com>
2022-07-20 15:31:59 -0400

    login search click people

--- main.py
+++ main.py
@@ -1,40 +1,104 @@
+from pip import main
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from bs4 import BeautifulSoup
+
+import bs4findpath
+
 import time
+
+
+# waiting helper functions
+def wait(s):
+    time.sleep(s)
+
+def wait_then_close(driver_arg, s):
+    time.sleep(s)
+    driver_arg.close()
+
+
+#log in function definition
+def linkedin_log_in( driver_p, email, pswd):
+    
+
+    #opening linkedIn webpage
+    driver_p.get("https://linkedin.com/uas/login")
+
+    wait(3)
+    # entering username
+    username = driver_p.find_element(By.ID, "username")
+    username.send_keys(email)
+
+    # entering password
+    pword = driver_p.find_element(By.ID, "password")
+    pword.send_keys(pswd)
+
+    # Clicking on the log in button
+    driver_p.find_element(By.XPATH, "//button[@type='submit']").click()
+
+
+    
+
+
+def searchPeople(driver_p, keywords):
+    search_bar_element = driver_p.find_element(By.CLASS_NAME, "search-global-typeahead__input")
+    search_bar_element.send_keys(keywords)
+    search_bar_element.send_keys(Keys.RETURN)
+    wait(2)
+    peopleButton = driver_p.find_element(By.XPATH, "//button[text()='People']")
+    peopleButton.click()
+
+
+
+#********************************  CONSTANT VARIABLES  ********************************
+#**************************************************************************************
 
 # Creating a webdriver instance
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+
+
+
+
+#********************************  TESTING  ********************************
+#***************************************************************************
+
+
+#*****************  Log in test  ***************
+#***********************************************
+
 # This instance will be used to log into LinkedIn
+main_driver = webdriver.Chrome(PATH)
+email = "m_regimbald@hotmail.com"
+pswd = "Regma1182!"
+linkedin_log_in( main_driver, email, pswd)
 
-# Opening linkedIn's login page
+searchPeople(main_driver, "developer")
 
-driver.get("https://linkedin.com/uas/login")
 
-# waiting for the page to load
-time.sleep(5)
+doc = BeautifulSoup(main_driver.page_source, "html.parser" )
+print(doc.prettify())
 
-# entering username
-username = driver.find_element(By.ID, "username")
 
-# In case of an error, try changing the element
-# tag used here.
+#wait_then_close(main_driver,5)
 
-# Enter Your Email Address
-username.send_keys("m_regimbald@hotmail.com")
 
-# entering password
-pword = driver.find_element(By.ID, "password")
-# In case of an error, try changing the element 
-# tag used here.
 
-# Enter Your Password
-pword.send_keys("Regma1182!")
 
-# Clicking on the log in button
-# Format (syntax) of writing XPath --> 
-# //tagname[@attribute='value']
-driver.find_element(By.XPATH, "//button[@type='submit']").click()
-# In case of an error, try changing the
-# XPath used here.
+
+
+
+
+
+
+
+
+
+
+
+#*****************  Output  ***************
+#***********************************************
+
+# person = {"name": "", "link":"" , "location": "" , "bio": "", "description_keywords":[],  }
+# output =[]
